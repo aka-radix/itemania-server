b8afd2919c2b7ead86ec5b70dbf2ab4c4fdd4d81
aka-radix <h.radiahmad@gmail.com>
2024-04-26 22:45:50 +0300

    chore: remove django-silk

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,16 +1,5 @@
 from .base import *  # noqa: F403
-from .base import INSTALLED_APPS, MIDDLEWARE
 
 # GENERAL
 
 DEBUG = True
-
-
-# APPS
-
-INSTALLED_APPS += ["silk", "django_extensions"]
-
-
-# MIDDLEWARE
-
-MIDDLEWARE += ["silk.middleware.SilkyMiddleware"]
