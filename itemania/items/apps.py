56ecaa4f4c4208e8a07cc12effd575016794db8b
aka-radix <h.radiahmad@gmail.com>
2024-04-27 17:16:29 +0300

    remove ready method from items app

--- itemania/items/apps.py
+++ itemania/items/apps.py
@@ -4,10 +4,3 @@ from django.apps import AppConfig
 class ItemsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "itemania.items"
-
-    def ready(self):
-        from .model_factories import ItemFactory
-        from .models import Item
-
-        if not Item.objects.exists():
-            Item.objects.bulk_create(ItemFactory.create_batch(10))
